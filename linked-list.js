c754b4f38317b2d0a1635a8c2f8af22abd81e45d
tptam <99264397+tptam@users.noreply.github.com>
2024-08-04 15:26:12 +0900

    Add skeleton LinkedList class

---
+++ linked-list.js
@@ -0,0 +1,21 @@
+import { createNode } from "./node";
+
+class LinkedList {
+  constructor() {}
+  append(value) {}
+  prepend(value) {}
+  size() {}
+  head() {}
+  tail() {}
+  at(index) {}
+  pop() {}
+  contains(value) {}
+  find(value) {}
+  toString() {}
+
+  //   Extra credit
+  insertAt(value, index) {}
+  removeAt(index) {}
+}
+
+export { LinkedList };
